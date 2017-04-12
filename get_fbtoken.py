3ca60018e0497c4dda350cfc6c88d21691479fc6
HermitSage <dth.bht@gmail.com>
2017-04-13 01:02:59 +0530

    Added Posts

--- get_fbtoken.py
+++ get_fbtoken.py
@@ -7,7 +7,8 @@ client_secret = "d3166755d7f0053694a0a07f6dfedc52"
 def get_authentication_url():
 	perms = ['read_page_mailboxes',
 	    'manage_pages',
-	    'publish_pages']
+	    'publish_pages',
+	    'manage_notifications']
 	canvas_url = 'http://localhost:8080/fb/success'
 	fb_url = fb.auth_url(app_id=app_id,canvas_url=canvas_url,perms=perms)
 	return fb_url
@@ -18,7 +19,7 @@ def short_to_long_term_token(code):
 	url += "client_id="+client_id+"&"
 	
 	url += "client_secret=" + client_secret + "&"
-	url += "fb_exchange_token=" + access_token
+	url += "fb_exchange_token=" + code
 	url = url.strip()
 	r = requests.get(url)
 	contents = r.json()
@@ -34,10 +35,10 @@ def code_to_access_token(code):
 	access_token = contents['access_token']
 	return access_token
 	
-"""
 if __name__ == "__main__":
 	link = get_authentication_url()
 	print(link)
 	token = input()
-	print(longtermtoken(token))
-"""
+	st = code_to_access_token(token)
+	lt = short_to_long_term_token(st)
+	print(lt)
--- posts.py
+++ posts.py
@@ -2,8 +2,23 @@ import facebook as fb
 
 fb_profile_id = "adithyabhatk"
 
+class Post():
+	def __init__(self):
+		self.title 	= ""
+		self.id 	= ""
+		self.msg 	= ""
+
 def preprocess_data(data):
-  return data
+	posts = []
+	for item in data:
+  		temp = Post()
+  		if 'message' in item.keys():
+  			temp.msg = item['message']
+  		if 'story' in item.keys():
+  			temp.title = item['story']
+  		temp.id = item['id']
+  		posts.append(temp)
+  	return posts
 
 def get_posts(access_token):
   facebook_graph = fb.GraphAPI(oauth_access_token)
